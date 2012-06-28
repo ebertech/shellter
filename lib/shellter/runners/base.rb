33c7e88f59959af29fc70e669aa3f150253b8e64
Andrew Eberbach <andrew@ebertech.ca>
2012-06-28 19:56:45 -0400

    forgot about inheritance of Base

--- lib/shellter/runners/base.rb
+++ lib/shellter/runners/base.rb
@@ -1,13 +1,14 @@
-require 'popen4'
-
 module Shellter
   module Runners
     class Base
       def initialize(options = {})
-        
+
       end
-      
+
       def run(command, &block)
+        unless defined?(POpen4)
+          require 'popen4'
+        end
         POpen4.popen4(command, &block)
       end
     end
--- lib/shellter/version.rb
+++ lib/shellter/version.rb
@@ -1,3 +1,3 @@
 module Shellter
-  VERSION = "0.9.3"
+  VERSION = "0.9.4"
 end
